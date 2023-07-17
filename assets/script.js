0c60a229b158e7f161c47a89f678770fcf571af8
Josh <72828296+JoshAtticus@users.noreply.github.com>
2023-07-18 01:56:07 +0800

    please work

--- assets/script.js
+++ assets/script.js
@@ -1,80 +1,55 @@
 $(document).ready(function() {
-    var resizing = false;
-    var resizeStartWidth, resizeStartHeight;
-    var defaultWidth = 800;
-    var defaultHeight = 600;
-
-    $(".app-window").draggable({
-        handle: ".window-bar",
-        containment: ".desktop",
-        start: function() {
-            resizing = false;
-            resizeStartWidth = $(this).width();
-            resizeStartHeight = $(this).height();
-        }
-    }).resizable({
-        containment: ".desktop",
-        minHeight: 200,
-        minWidth: 200,
-        resize: function(event, ui) {
-            if (resizing) {
-                var widthDelta = ui.size.width - resizeStartWidth;
-                var heightDelta = ui.size.height - resizeStartHeight;
-
-                $(this).width(resizeStartWidth + widthDelta);
-                $(this).height(resizeStartHeight + heightDelta);
-            } else {
-                resizing = true;
-                resizeStartWidth = $(this).width();
-                resizeStartHeight = $(this).height();
-            }
-        }
-    }).on("resize", function() {
-        var desktopHeight = $(".desktop").height();
-        var windowHeight = $(this).height();
-        var windowWidth = $(this).width();
-        if (windowHeight > desktopHeight) {
-            $(this).height(desktopHeight);
-        }
-        if (windowWidth > $(".desktop").width()) {
-            $(this).width($(".desktop").width());
-        }
-    });
-
     $(".app-link").click(function() {
         var url = $(this).data("url");
         var appTitle = $(this).data("title");
+        var appWindow = $("<div>").addClass("app-window resizable draggable").appendTo(".desktop");
+        var windowBar = $("<div>").addClass("window-bar").appendTo(appWindow);
+        var closeButton = $("<div>").addClass("red-circle").appendTo(windowBar);
+        var maximizeButton = $("<div>").addClass("green-circle").appendTo(windowBar);
+        var title = $("<div>").addClass("title").text(appTitle).appendTo(windowBar);
+        var iframe = $("<iframe>").addClass("app-iframe").attr("src", url).appendTo(appWindow);
 
-        $(".app-iframe").attr("src", url);
-        $(".app-window").addClass("open");
-        $(".title").text(appTitle);
+        closeButton.click(function() {
+            appWindow.remove();
+        });
+
+        maximizeButton.click(function() {
+            appWindow.toggleClass("maximized");
+        });
+
+        appWindow.draggable({
+            handle: windowBar,
+            containment: ".desktop",
+            scroll: false
+        });
+
+        appWindow.resizable({
+            handles: "n, e, s, w, ne, se, sw, nw",
+            containment: ".desktop",
+            minWidth: 200,
+            minHeight: 200
+        });
     });
 
-    $(".red-circle").click(function() {
-        $(".app-window").removeClass("open");
-        $(".app-iframe").attr("src", "");
-    });
+    function updateTime() {
+        var currentDate = new Date();
+        var hours = currentDate.getHours();
+        var minutes = currentDate.getMinutes();
+        var ampm = hours >= 12 ? 'PM' : 'AM';
 
-    $(".green-circle").click(function() {
-        var $window = $(this).closest(".app-window");
-        var $desktop = $window.closest(".desktop");
+        // Convert to 12-hour format
+        hours = hours % 12 || 12;
 
-        if ($window.hasClass("maximized")) {
-            $window.removeClass("maximized");
-            $window.css({
-                width: defaultWidth,
-                height: defaultHeight,
-                top: ($desktop.height() - defaultHeight) / 2,
-                left: ($desktop.width() - defaultWidth) / 2
-            });
-        } else {
-            $window.addClass("maximized");
-            $window.css({
-                width: $desktop.width(),
-                height: $desktop.height(),
-                top: 0,
-                left: 0
-            });
-        }
-    });
+        // Add leading zeros to minutes if less than 10
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+
+        // Update the clock display
+        $('.clock').text(hours + ':' + minutes + ' ' + ampm);
+    }
+
+    // Call updateTime initially
+    updateTime();
+
+    // Update time every second
+    setInterval(updateTime, 1000);
 });

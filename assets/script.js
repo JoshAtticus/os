fc1b8f0e98e8d69c78d4c6785f39a6b1ce1178fc
Josh <72828296+JoshAtticus@users.noreply.github.com>
2023-07-18 02:26:02 +0800

    alright, working clock?

--- assets/script.js
+++ assets/script.js
@@ -30,4 +30,28 @@ $(document).ready(function() {
         minWidth: 200,
         minHeight: 200
     });
+
+    // Function to update the time every second
+    function updateTime() {
+        var currentTime = new Date();
+        var hours = currentTime.getHours();
+        var minutes = currentTime.getMinutes();
+        var seconds = currentTime.getSeconds();
+        var meridiem = hours >= 12 ? 'PM' : 'AM';
+
+        // Convert the hours to 12-hour format
+        hours = hours % 12 || 12;
+
+        // Add leading zeros if necessary
+        hours = hours < 10 ? '0' + hours : hours;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+
+        // Update the content of the <h1> element
+        var timeString = hours + ':' + minutes + ':' + seconds + ' ' + meridiem;
+        document.getElementById('current-time').innerHTML = timeString;
+      }
+
+      // Call updateTime function every second
+      setInterval(updateTime, 1000);
 });

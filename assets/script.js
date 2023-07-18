23ac2b7ec4def84d0a311eb605cc41ac6c7911f9
Josh <72828296+JoshAtticus@users.noreply.github.com>
2023-07-18 10:53:35 +0800

    I fixed them

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,46 @@
 $(document).ready(function() {
+    var resizing = false;
+    var resizeStartWidth, resizeStartHeight;
+    var defaultWidth = 800;
+    var defaultHeight = 600;
+
+    $(".app-window").draggable({
+        handle: ".window-bar",
+        containment: ".desktop",
+        start: function() {
+            resizing = false;
+            resizeStartWidth = $(this).width();
+            resizeStartHeight = $(this).height();
+        }
+    }).resizable({
+        containment: ".desktop",
+        minHeight: 200,
+        minWidth: 200,
+        resize: function(event, ui) {
+            if (resizing) {
+                var widthDelta = ui.size.width - resizeStartWidth;
+                var heightDelta = ui.size.height - resizeStartHeight;
+
+                $(this).width(resizeStartWidth + widthDelta);
+                $(this).height(resizeStartHeight + heightDelta);
+            } else {
+                resizing = true;
+                resizeStartWidth = $(this).width();
+                resizeStartHeight = $(this).height();
+            }
+        }
+    }).on("resize", function() {
+        var desktopHeight = $(".desktop").height();
+        var windowHeight = $(this).height();
+        var windowWidth = $(this).width();
+        if (windowHeight > desktopHeight) {
+            $(this).height(desktopHeight);
+        }
+        if (windowWidth > $(".desktop").width()) {
+            $(this).width($(".desktop").width());
+        }
+    });
+
     $(".app-link").click(function() {
         var url = $(this).data("url");
         var appTitle = $(this).data("title");
@@ -6,32 +48,33 @@ $(document).ready(function() {
         $(".app-iframe").attr("src", url);
         $(".app-window").addClass("open");
         $(".title").text(appTitle);
-        $(".app-window").removeClass("maximized"); // Add this line to remove the maximized class
     });
 
     $(".red-circle").click(function() {
         $(".app-window").removeClass("open");
         $(".app-iframe").attr("src", "");
-        $(".title").text("");
     });
 
     $(".green-circle").click(function() {
-        $(".app-window").toggleClass("maximized");
-    });
+        var $window = $(this).closest(".app-window");
+        var $desktop = $window.closest(".desktop");
 
-    $(".app-window").draggable({
-        handle: ".window-bar",
-        containment: "body",
-        scroll: false
-    });
-
-    $(".app-window").resizable({
-        handles: "n, e, s, w, ne, se, sw, nw",
-        containment: "body",
-        minWidth: 200,
-        minHeight: 200,
-        start: function(event, ui) {
-            $(this).removeClass("maximized"); // Add this line to remove the maximized class when resizing starts
+        if ($window.hasClass("maximized")) {
+            $window.removeClass("maximized");
+            $window.css({
+                width: defaultWidth,
+                height: defaultHeight,
+                top: ($desktop.height() - defaultHeight) / 2,
+                left: ($desktop.width() - defaultWidth) / 2
+            });
+        } else {
+            $window.addClass("maximized");
+            $window.css({
+                width: $desktop.width(),
+                height: $desktop.height(),
+                top: 0,
+                left: 0
+            });
         }
     });
 });

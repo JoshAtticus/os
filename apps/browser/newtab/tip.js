6e067fb181a164df40fa65c09cd7b6ef915162e9
Josh <72828296+JoshAtticus@users.noreply.github.com>
2023-08-11 18:31:37 +0800

    yes

--- apps/browser/newtab/tip.js
+++ apps/browser/newtab/tip.js
@@ -4,7 +4,8 @@
         "Type ../../ into the URL bar and click go to open another AtticusOS session in this browser",
         "Want to learn more about CORS in this browser and how to disable it? Type <b>corsinfo</b> into the URL bar and click go.",
         "Google doesn't work in this browser due to CORS. Instead, you can use Bing, or if you also want Google search results, use https://search.tosdr.org",
-        "Want to use Scratch? We recommend https://penguinmod.site, it has more features and works in this browser!"
+        "Want to use Scratch? We recommend https://penguinmod.site, it has more features and works in this browser!",
+        "If your browser has sync functionalities, your AtticusOS preferences will sync across devices."
       ];
 
       // Function to display a random phrase
